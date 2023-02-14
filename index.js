e34ec617034da43a6a8813756b1ae6a47f0f3311
Shreyas <rasaikarshreyas21@gmail.com>
2023-02-14 15:35:50 +0530

    fixed a bug4

--- index.js
+++ index.js
@@ -16,12 +16,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.URL,
-    credentials: true,
-  })
-);
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 connectDb();
