cfcd2308687facc72656363923928cf5d3bd0e45
Shreyas <rasaikarshreyas21@gmail.com>
2023-02-14 13:12:48 +0530

    fixed a bug2

--- index.js
+++ index.js
@@ -16,7 +16,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.URL,
+  })
+);
 app.use(express.json());
 
 connectDb();
@@ -31,5 +35,5 @@ app.use("/api/", bookingRoomRoute);
 app.use("/api/", bookingHallRoute);
 
 app.listen(process.env.PORT, () => {
-  console.log("Server is running on port 3000");
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
